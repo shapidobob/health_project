7596df94217f445628c255c28ec9a2e77c1f08e3
shapidobob <shapidobob@gmail.com>
2024-04-10 22:30:28 -0400

    added functions

--- project_2.py
+++ project_2.py
@@ -1,5 +1,6 @@
 import psycopg2
 import pymysql
+from datetime import datetime
 
 # database connection
 #connection = pymysql.connect(host="localhost", port=8889, user="root", passwd="root", database="SELECTION_DB")
@@ -8,14 +9,14 @@ import pymysql
 #connection.close()
 
 def connect(dtb, usr, hst, password, port):
-    connection = pymysql.connect(host=hst, port=port, user=usr, passwd=password, database=dtb)
-    return connection
-    """conn = psycopg2.connect(database = dtb, 
+    #connection = pymysql.connect(host=hst, port=port, user=usr, passwd=password, database=dtb)
+    #return connection
+    conn = psycopg2.connect(database = dtb, 
                             user = usr, 
                             host= hst,
                             password = password,
                             port = port)
-    return conn"""
+    return conn
 
 def addmember(conn,Fname, Lname, Birthday, Weight, Resting_Heart_Rate, Blood_Pressure, Payment_Info, Email):
     cur = conn.cursor()
@@ -39,7 +40,7 @@ def addgoal(conn, Member_ID, Metric, Value):
     conn.commit()
     cur.close()
 
-def updategoal(Goal_ID, Metric, Value):
+def updategoal(conn,Goal_ID, Metric, Value):
     cur = conn.cursor()
     cmd = "UPDATE members SET Metric = %s, value = %s WHERE Goal_ID = %s"
     cur.execute(cmd, (Metric, Value, Goal_ID))
@@ -53,9 +54,94 @@ def addactivity(conn, Member_ID, Metric, Value):
     conn.commit()
     cur.close()
 
+def addtrainer(conn, Fname, Lname, Email):
+    cur = conn.cursor()
+    cmd = "INSERT INTO trainers(Fname, Lname, Email) VALUES(%s,%s,%s);"
+    cur.execute(cmd, (Fname, Lname, Email))
+    conn.commit()
+    cur.close()
 
-if __name__ == "__main__":
-    conn = connect("health_center","root","localhost","",3306)
-    addmember(conn,"Fname", "Lname", '2008-11-11', "200", "70", "20", "testing", "email")
-    updatemember(conn,"newname", "Lname", '2008-11-11', "190", "70", "20", "testing", "email", 1)
-    
+
+def addsession(conn, Room_Number, Instructor_ID, Start_Time, End_Time, Activity, Capacity):
+    cur = conn.cursor()
+    cmd = "SELECT * FROM sessions WHERE Instructor_ID = %s OR Room_Number = %s;"
+    cur.execute(cmd, (Instructor_ID,Room_Number))
+    rows = cur.fetchall()
+    conn.commit()
+    for row in rows:
+        if (row[3] <= datetime.strptime(End_Time, '%Y-%m-%d %H:%M:%S')) and (row[3] >= datetime.strptime(Start_Time, '%Y-%m-%d %H:%M:%S')):
+            return False
+        elif(row[4] <= datetime.strptime(End_Time, '%Y-%m-%d %H:%M:%S')) and (row[4] >= datetime.strptime(Start_Time, '%Y-%m-%d %H:%M:%S')):
+            return False
+    cur = conn.cursor()
+    cmd = "INSERT INTO sessions(Room_Number, Instructor_ID, Start_Time, End_Time, Activity, Capacity) VALUES(%s,%s,%s,%s,%s,%s);"
+    cur.execute(cmd, (Room_Number, Instructor_ID, Start_Time, End_Time, Activity, Capacity))
+    conn.commit()
+    cur.close()
+    return True
+
+def changesession(conn, Room_Number, Instructor_ID, Start_Time, End_Time, Activity, Capacity, Session_ID):
+    cur = conn.cursor()
+    cmd = "SELECT * FROM sessions WHERE Instructor_ID = %s OR Room_Number = %s;"
+    cur.execute(cmd, (Instructor_ID,Room_Number))
+    rows = cur.fetchall()
+    conn.commit()
+    for row in rows:
+        if (row[3] <= datetime.strptime(End_Time, '%Y-%m-%d %H:%M:%S')) and (row[3] >= datetime.strptime(Start_Time, '%Y-%m-%d %H:%M:%S')):
+            return False
+        elif(row[4] <= datetime.strptime(End_Time, '%Y-%m-%d %H:%M:%S')) and (row[4] >= datetime.strptime(Start_Time, '%Y-%m-%d %H:%M:%S')):
+            return False
+    cur = conn.cursor()
+    cmd = "UPDATE sessions SET Room_Number=%s, Instructor_ID=%s, Start_Time=%s, End_Time=%s, Activity=%s, Capacity=%s WHERE Session_ID = %s;"
+    cur.execute(cmd, (Room_Number, Instructor_ID, Start_Time, End_Time, Activity, Capacity, Session_ID))
+    conn.commit()
+    cur.close()
+    return True
+
+def searchmembers(conn, Fname, Lname):
+    cur = conn.cursor()
+    cmd = "SELECT * FROM members WHERE Fname = %s AND Lname = %s;"
+    cur.execute(cmd, (Fname,Lname))
+    rows = cur.fetchall()
+    conn.commit()
+    return rows
+
+def getpayment(conn, Fname, Lname):
+    cur = conn.cursor()
+    cmd = "SELECT Payment_Info FROM members WHERE Fname = %s AND Lname = %s;"
+    cur.execute(cmd, (Fname,Lname))
+    rows = cur.fetchall()
+    conn.commit()
+    return rows
+
+def booksession(conn, Member_ID, Session_ID):
+    cur = conn.cursor()
+    cmd = "SELECT * FROM sessions WHERE Session_ID = %s;"
+    cur.execute(cmd, (Session_ID))
+    rows = cur.fetchall()
+    conn.commit()
+    if len(rows) == 0:
+        return False
+    for row in rows:
+        cap = row[6]
+    cur = conn.cursor()
+    cmd = "SELECT * FROM bookings WHERE Session_ID = %s;"
+    cur.execute(cmd, (Session_ID))
+    rows = cur.fetchall()
+    conn.commit()
+    if len(rows) == cap:
+        return False
+    cur = conn.cursor()
+    cmd = "INSERT INTO bookings(Member_ID, Session_ID) VALUES(%s,%s);"
+    cur.execute(cmd, (Member_ID,Session_ID))
+    conn.commit()
+    cur.close()
+    return True
+
+def checkmaintenance(conn):
+    cur = conn.cursor()
+    cmd = "SELECT * FROM machines ORDER BY Last_Maintenance ASC;"
+    cur.execute(cmd)
+    rows = cur.fetchall()
+    conn.commit()
+    return rows
